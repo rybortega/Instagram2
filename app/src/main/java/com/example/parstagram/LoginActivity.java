d0e4a500b6ea2feecf2a4709ea532e58ffe2373d
uuilmer <wilmerh701@gmail.com>
2020-07-06 14:30:06 -0700

    Added Login: User can sign in

---
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.example.parstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername;
+    EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if(ParseUser.getCurrentUser() != null)
+            goToMain();
+
+        etUsername = findViewById(R.id.entered_username);
+        etPassword = findViewById(R.id.entered_password);
+
+        (findViewById(R.id.login_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logInInBackground(etUsername.getText().toString(), etPassword.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(e != null){
+                            Toast.makeText(LoginActivity.this, "Wrong Username/ Password", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        goToMain();
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void goToMain() {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
